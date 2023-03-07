edb6e284bbc08e07e2d88d7aa3c49bbbab5031ac
Matthew Baty <mattbaty@outlook.com>
2023-03-06 21:59:29 -0500

    map on page, now need to write marker loop to use fetch data and populate map

--- index.js
+++ index.js
@@ -1,133 +1,131 @@
+$(function () {
 
-// Search meals by name:
-function searchByName(meal) {
-    fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=" + meal)
-        .then(function (response) {
-            return response.json()
-        })
-        .then(function (data) {
-            // Console Logging all of the data
-            console.log(data);
+console.log(L)
 
-            var mealName = data.meals[0].strMeal;
-            console.log(mealName);
+    // Search meals by name:
+    function searchByName(meal) {
+        fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=" + meal)
+            .then(function (response) {
+                return response.json()
+            })
+            .then(function (data) {
+                // Console Logging all of the data
+                console.log(data);
 
-            var areaOfOrigin = data.meals[0].strArea;
-            console.log(areaOfOrigin);
+                var mealName = data.meals[0].strMeal;
+                console.log(mealName);
 
-            
-        })
-}
-// searchByName("Duck Confit");
+                var areaOfOrigin = data.meals[0].strArea;
+                console.log(areaOfOrigin);
 
 
-// Function which will return random meal
-// This function needs to be paired with the button which will generate a random recipe
-function singleRandomMeal() {
-    fetch("https://www.themealdb.com/api/json/v1/1/random.php")
-        .then(function (response) {
-            return response.json()
-        })
-        .then(function (data) {
-            var mealData = data.meals[0];
-            console.log(mealData);
+            })
+    }
+    // searchByName("Duck Confit");
 
-            // Loop which logs all of the ingredients which are not blank
-            // There are a max of 20 ingredients - some of which are blankk
-            // WIP
+3
+    // Function which will return random meal
+    // This function needs to be paired with the button which will generate a random recipe
+    function singleRandomMeal() {
+        fetch("https://www.themealdb.com/api/json/v1/1/random.php")
+            .then(function (response) {
+                return response.json()
+            })
+            .then(function (data) {
+                var mealData = data.meals[0];
+                console.log(mealData);
 
-            // Same thing needs to be done for the measures - and then the two of them need to be linked together
-            for (var i = 0; i < 20; i++) {
-                if (mealData[["strIngredient"+i]] !== "") {
-                    console.log(mealData["strIngredient"+i]);
+                // Loop which logs all of the ingredients which are not blank
+                // There are a max of 20 ingredients - some of which are blankk
+                // WIP
+
+                // Same thing needs to be done for the measures - and then the two of them need to be linked together
+                for (var i = 0; i < 20; i++) {
+                    if (mealData[["strIngredient" + i]] !== "") {
+                        console.log(mealData["strIngredient" + i]);
+                    }
                 }
-            }
 
-        })
+            })
 
-}
-singleRandomMeal();
+    }
+    singleRandomMeal();
 
-// List all ingredients
-function ingredients(){
-    var url = "https://www.themealdb.com/api/json/v1/1/list.php?i=list";
+    // List all ingredients
+    function ingredients() {
+        var url = "https://www.themealdb.com/api/json/v1/1/list.php?i=list";
 
-    fetch(url)
-    .then(function (response) {
-        return response.json()
-    })
-    .then(function (data) {
-        console.log("All Ingredients Data:");
-        console.log(data);
+        fetch(url)
+            .then(function (response) {
+                return response.json()
+            })
+            .then(function (data) {
+                console.log("All Ingredients Data:");
+                console.log(data);
 
-    })
-}
+            })
+    }
 
-// Search meals by first letter:
-function searchByFirstLetter(x) {
-    fetch("https://www.themealdb.com/api/json/v1/1/search.php?f=" + x)
-        .then(function (response) {
-            return response.json()
-        })
-        .then(function (data) {
-            console.log("Searching meals by the first letter:");
+    // Search meals by first letter:
+    function searchByFirstLetter(x) {
+        fetch("https://www.themealdb.com/api/json/v1/1/search.php?f=" + x)
+            .then(function (response) {
+                return response.json()
+            })
+            .then(function (data) {
+                console.log("Searching meals by the first letter:");
 
-            var len = data.meals.length;
-            console.log(len);
-            console.log(data);
+                var len = data.meals.length;
+                console.log(len);
+                console.log(data);
 
-            for (var i = 0; i < len; i++) {
-                console.log("Meal Name: " + data.meals[i].strMeal)
-            }
-        })
-}
-// searchByFirstLetter("i");
+                for (var i = 0; i < len; i++) {
+                    console.log("Meal Name: " + data.meals[i].strMeal)
+                }
+            })
+    }
+    // searchByFirstLetter("i");
 
-// Meal Details by ID
+    // Meal Details by ID
 
-function mealByID(id) {
-    fetch("https://www.themealdb.com/api/json/v1/1/lookup.php?i="+id)
-        .then(function (response) {
-            return response.json()
-        })
-        .then(function (data) {
-            console.log(data);
-        })
-}
-// mealByID(52907);
+    function mealByID(id) {
+        fetch("https://www.themealdb.com/api/json/v1/1/lookup.php?i=" + id)
+            .then(function (response) {
+                return response.json()
+            })
+            .then(function (data) {
+                console.log(data);
+            })
+    }
+    // mealByID(52907);
 
-// start of maps api
+    // start of maps api
 
-var requestOptions = {
-    method: 'GET',
-  };
-  
-  fetch("https://api.geoapify.com/v2/places?categories=commercial.supermarket&filter=rect%3A10.716463143326969%2C48.755151258420966%2C10.835314015356737%2C48.680903341613316&limit=20&apiKey=fd3d5e013b4b4cea96e30a0054594a65", requestOptions)
-    .then(response => response.json())
-    .then(result => console.log(result))
-    .catch(error => console.log('error', error));
+    var map = L.map("my-map").setView([35.2271, -80.8431], 14);
 
-fetch("https://api.geoapify.com/v2/places?categories=commercial.supermarket&filter=place:51ad15dd56663554c0591d95d55d619a4140f00101f90107b5020000000000c00206920309436861726c6f747465&lang=en&limit=30&apiKey=fd3d5e013b4b4cea96e30a0054594a65")
-        .then(response => response.json())
-        .then(result => {
-          // create a new map object
-          const map = new Geoapify.Map({
-            apiKey: 'fd3d5e013b4b4cea96e30a0054594a65',
-            basemap: 'bright-v9',
-            container: 'map-container',
-            // center the map on the first result
-            center: [result.features[0].properties.lon, result.features[0].properties.lat], 
-            // set the initial zoom level
-            zoom: 13 
-          });
+      // Get your own API Key on https://myprojects.geoapify.com
+      var myAPIKey = "fd3d5e013b4b4cea96e30a0054594a65";
 
-          // add a marker for each result
-          result.features.forEach(feature => {
-            const marker = new Geoapify.Marker({
-              coordinates: [feature.properties.lon, feature.properties.lat],
-              color: 'blue'
-            });
-            map.addMarker(marker);
-          });
-        })
-        .catch(error => console.log('error', error));
+      // Retina displays require different mat tiles quality
+      var isRetina = L.Browser.retina;
+
+      var baseUrl =
+        "https://maps.geoapify.com/v1/tile/osm-bright/{z}/{x}/{y}.png?apiKey={apiKey}";
+      var retinaUrl =
+        "https://maps.geoapify.com/v1/tile/osm-bright/{z}/{x}/{y}@2x.png?apiKey={apiKey}";
+
+      // Add map tiles layer. Set 20 as the maximal zoom and provide map data attribution.
+      L.tileLayer(isRetina ? retinaUrl : baseUrl, {
+        attribution:
+          'Powered by <a href="https://www.geoapify.com/" target="_blank">Geoapify</a> | © OpenStreetMap <a href="https://www.openstreetmap.org/copyright" target="_blank">contributors</a>',
+        apiKey: myAPIKey,
+        maxZoom: 14,
+        id: "osm-bright",
+      }).addTo(map);
+
+    //   fetch("https://api.geoapify.com/v2/places?categories=commercial.supermarket&filter=place:51ad15dd56663554c0591d95d55d619a4140f00101f90107b5020000000000c00206920309436861726c6f747465&lang=en&limit=30&apiKey=fd3d5e013b4b4cea96e30a0054594a65")
+    //   .then(response => response.json())
+    //   .then(result => console.log(result))
+    //   .catch(error => console.log('error', error));
+
+})
