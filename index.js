539d57180b16f7da6e202b26902f385206ef1dc7
CJMoye <Cjmoye30@gmail.com>
2023-03-12 12:11:42 -0400

    Adding code to narrow down search results to 5 at a time

--- index.js
+++ index.js
@@ -32,21 +32,21 @@ $(function () {
         });
 
         fetch("https://api.geoapify.com/v2/places?categories=commercial.supermarket&filter=place:51ad15dd56663554c0591d95d55d619a4140f00101f90107b5020000000000c00206920309436861726c6f747465&lang=en&limit=20&apiKey=fd3d5e013b4b4cea96e30a0054594a65")
-        .then((response) => response.json())
-        .then((result) => {
-          console.log(result);
-          result.features.forEach((feature) => {
-            let markerPopup = L.popup().setContent(
-              `<div>${feature.properties.address_line1}</div><div>${feature.properties.address_line2}</div>`
-            );
-            let marker = L.marker(
-              [feature.properties.lat, feature.properties.lon],
-              { icon: markerIcon }
-            )
-              .bindPopup(markerPopup).addTo(map);
-          });
-        })
-        .catch((error) => console.log("error", error));
+            .then((response) => response.json())
+            .then((result) => {
+                console.log(result);
+                result.features.forEach((feature) => {
+                    let markerPopup = L.popup().setContent(
+                        `<div>${feature.properties.address_line1}</div><div>${feature.properties.address_line2}</div>`
+                    );
+                    let marker = L.marker(
+                        [feature.properties.lat, feature.properties.lon],
+                        { icon: markerIcon }
+                    )
+                        .bindPopup(markerPopup).addTo(map);
+                });
+            })
+            .catch((error) => console.log("error", error));
     }
 
     // end of map api
@@ -64,11 +64,11 @@ $(function () {
         console.log(userRecipe);
         searchByName(userRecipe);
     })
-    
+
     // Event handler tied to the newly created buttons if there is more than 1 option available for the recipe that they entered
     // Grabbing the ID from the target of the element of the button we are clicking
     // Using that ID to then pull the recipe contents from the ID function 
-    $("#recipe-results").click(function (e) {
+    $("#search-results-container").click(function (e) {
         var buttonID = e.target.id;
 
         // Send the user value to local storage
@@ -76,13 +76,14 @@ $(function () {
         searchHistory.push(buttonID);
         localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
 
-        setTimeout(function(){
+        setTimeout(function () {
             location.href = "landingPage.html";
         }, 250)
 
     })
 
     // Fetch recipe by name
+    // Should limit the search results to 5, and then have a button to search for more which would increase the index by 5
     function searchByName(meal) {
         fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=" + meal)
             .then(function (response) {
@@ -92,15 +93,31 @@ $(function () {
                 // Console Logging all of the data
 
                 var recipeOptionsNum = data.meals.length;
+                var searchIndex = 0;
                 console.log(recipeOptionsNum);
 
-                for (var i = 0; i < recipeOptionsNum; i++) {
-                    var idMeal = data.meals[i].idMeal;
-                    var mealOptionEl = $("<button>").text(data.meals[i].strMeal).attr("id", idMeal).addClass("recipe-option");
-                    $("#recipe-results").append(mealOptionEl);
+
+                // Wrap the for loop in a function and if a button is clicked, then run the fuction again
+
+                // Show more options
+                $("#show-more").click(function () {
+                    showOptions()
+                })
+
+                function showOptions() {
+                    for (var i = 0; i < recipeOptionsNum; i += 5) {
+                        var idMeal = data.meals[i].idMeal;
+                        var mealOptionEl = $("<button>").text(data.meals[i].strMeal).attr("id", idMeal).addClass("recipe-option");
+                        $("#search-results-container").append(mealOptionEl);
+                        searchIndex++;
+                        $("#results-qty").text(searchIndex + " out of " + recipeOptionsNum);
+                    }
                 }
+                showOptions();
             })
     }
+
+
 
     // Fetch random recipe
     function singleRandomMeal() {
@@ -119,19 +136,20 @@ $(function () {
     }
 
     var searchHistory = [];
-    function initStorage(){
+    function initStorage() {
         var storedRecipes = JSON.parse(localStorage.getItem("searchHistory"));
-        if(storedRecipes !== null) {
+        if (storedRecipes !== null) {
             searchHistory = storedRecipes;
             console.log(searchHistory);
         }
+
     }
 
     // Event listeners for search and random generate button
     // Adding a delay so that the function can run before transitioning to the next page
     $("#random-button-search").click(function () {
         singleRandomMeal();
-        setTimeout(function(){
+        setTimeout(function () {
             location.href = "landingPage.html";
         }, 250)
     })
